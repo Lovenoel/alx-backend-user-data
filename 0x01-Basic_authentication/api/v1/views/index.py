e45d4a6324ee468d08fd7a2ab5e26377a0a5120e
lovenoel <kinglovenoel@gmail.com>
2024-11-16 04:22:32 +0300

    Basic authentication

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -2,8 +2,9 @@
 """ Module of Index views
 """
 from flask import jsonify, abort
-from api.v1.views import app_views
+from flask import Blueprint
 
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
