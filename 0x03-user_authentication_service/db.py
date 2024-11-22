554f96e0b5f718b93aecb8d91a04d54b34930dbf
lovenoel <kinglovenoel@gmail.com>
2024-11-22 03:11:12 +0300

    Added user authentication code

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,7 @@ Database class that implements the user opeartions and methods.
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
@@ -12,10 +13,22 @@ from user import Base, User
 class DB:
     """Database class for handling user operations"""
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
         self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
-        self.__session = sessionmaker(bind=self._engine)()
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database"""
