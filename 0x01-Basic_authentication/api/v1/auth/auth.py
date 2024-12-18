1a7b94a82317539ca3a09d348b3293744cf5e929
lovenoel <kinglovenoel@gmail.com>
2024-12-18 09:48:53 +0000

    Task 3 corrected

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,13 +16,13 @@ class Auth:
             if path.endswith(excluded_path):
                 return False
         return True
-    
+
     def authorization_header(self, request=None) -> str:
         """ Returns the Authorization header if present """
         if request is None or 'Authorization' not in request.headers:
             return None
         return request.headers['Authorization']
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ Returns the current user if authenticated """
         return None
